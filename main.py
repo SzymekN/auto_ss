94d828c6b1232d75e8287faf16c03a96ae07aebb
SzymekN <nowak.szymek00@gmail.com>
2022-02-23 15:31:26 +0100

    brzydki kodzik

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from PIL import ImageGrab
 from numpy import asarray
 import time
 import win32gui
+import win32api
+import win32ui
+import win32com
+from ctypes import windll
+import ctypes
+from PIL import Image as Image
+import pyautogui
 
 
 # def mse(imageA, imageB):
@@ -48,29 +55,83 @@ def enum_cb(hwnd, results):
 
 if __name__ == "__main__":
 
+    hwnd = win32gui.FindWindow(None, 'Michał Twaróg | Microsoft Teams')
+
+    win32gui.EnumWindows(enum_cb, toplist)
+    # print(winlist)
+    teams = [(hwnd, title) for hwnd, title in winlist if '| microsoft teams' in title.lower()]
+    for window in teams:
+        print(window)
+    # just grab the hwnd for first window matching teams
+    print(len(teams))
+    teams = teams[0]
+    print(teams)
+    hwnd = teams[0]
+
+    # Change the line below depending on whether you want the whole window
+    # or just the client area. 
+    #left, top, right, bot = win32gui.GetClientRect(hwnd)
+    # left, top, right, bot = win32gui.GetWindowRect(hwnd)
+    w, h = pyautogui.size()
+
+
+    hwndDC = win32gui.GetWindowDC(hwnd)
+    mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
+    saveDC = mfcDC.CreateCompatibleDC()
+
+    saveBitMap = win32ui.CreateBitmap()
+    saveBitMap.CreateCompatibleBitmap(mfcDC, w, h)
+
+    saveDC.SelectObject(saveBitMap)
+
+    # Change the line below depending on whether you want the whole window
+    # or just the client area. 
+    result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 3)
+    # result = windll.user32.PrintWindow(hwnd, saveDC.GetSafeHdc(), 0)
+    print (result)
+
+    # win32gui.SetForegroundWindow(hwnd)
+    bmpinfo = saveBitMap.GetInfo()
+    bmpstr = saveBitMap.GetBitmapBits(True)
+
+    im = Image.frombuffer(
+        'RGB',
+        (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
+        bmpstr, 'raw', 'BGRX', 0, 1)
+
+    win32gui.DeleteObject(saveBitMap.GetHandle())
+    saveDC.DeleteDC()
+    mfcDC.DeleteDC()
+    win32gui.ReleaseDC(hwnd, hwndDC)
+
+    if result == 1:
+        #PrintWindow Succeeded
+        im.show("test.png")
+
+
     # im1 = ImageGrab.grab()  # X1,Y1,X2,Y2
     # im1.save("ss0.png")
     # image_count = 0
 
     run = False
 
-    win32gui.EnumWindows(enum_cb, toplist)
-    # print(winlist)
-    firefox = [(hwnd, title) for hwnd, title in winlist if '| microsoft teams' in title.lower()]
-    for window in firefox:
-        print(window)
-    # just grab the hwnd for first window matching firefox
-    print(len(firefox))
-    firefox = firefox[0]
-    print(firefox)
-    hwnd = firefox[0]
+    # win32gui.EnumWindows(enum_cb, toplist)
+    # # print(winlist)
+    # firefox = [(hwnd, title) for hwnd, title in winlist if '| microsoft teams' in title.lower()]
+    # for window in firefox:
+    #     print(window)
+    # # just grab the hwnd for first window matching firefox
+    # print(len(firefox))
+    # firefox = firefox[0]
+    # print(firefox)
+    # hwnd = firefox[0]
 
 
-    win32gui.SetForegroundWindow(hwnd)
-    bbox = win32gui.GetWindowRect(hwnd)
-    print(bbox)
-    img = ImageGrab.grab(bbox)
-    img.show()
+    # win32gui.SetForegroundWindow(hwnd)
+    # bbox = win32gui.GetWindowRect(hwnd)
+    # print(bbox)
+    # img = ImageGrab.grab(bbox)
+    # img.show()
 
     # while run:
 
