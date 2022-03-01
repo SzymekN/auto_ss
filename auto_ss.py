1cecdce50808690871405e90f63fc5523e952f0a
SzymekN <nowak.szymek00@gmail.com>
2022-03-01 21:14:43 +0100

    menu, tweaks

--- auto_ss.py
+++ auto_ss.py
@@ -5,6 +5,7 @@ from numpy import asarray, count_nonzero
 from ctypes import windll
 from PIL import Image as Image
 
+
 def goodbye(auto_ss):
     '''Function executed at exit, saves counter to file'''
     try:
@@ -12,7 +13,7 @@ def goodbye(auto_ss):
             file.write(str(auto_ss.image_count))
     except:
         print("couldn't save")
-            
+
 
 class Auto_ss():
     '''Main class in script, handles finding wanted window and making screen shots'''
@@ -22,15 +23,15 @@ class Auto_ss():
 
         self.winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
 
-
     def _get_window_handle(self):
         '''Function gets handle to wanted window, and saves it'''
 
         # get all windows
         win32gui.EnumWindows(self.enum_cb, self.toplist)
-        
+
         # get windows with matching title
-        windows = [(hwnd, title) for hwnd, title in self.winlist if self.window_name in title.lower()]
+        windows = [(hwnd, title) for hwnd,
+                   title in self.winlist if self.window_name_to_search in title.lower()]
 
         # error if couldn't find any window
         if not windows:
@@ -43,15 +44,14 @@ class Auto_ss():
 
         # handle to wanted window
         self.hwnd = window[0]
-
+        self.window_name_found = window[1]
 
     def _create_context(self):
         '''Create new context from window handle'''
 
         self.hwndDC = win32gui.GetWindowDC(self.hwnd)
-        self.mfcDC  = win32ui.CreateDCFromHandle(self.hwndDC)
+        self.mfcDC = win32ui.CreateDCFromHandle(self.hwndDC)
         self.saveDC = self.mfcDC.CreateCompatibleDC()
-
 
     def take_screenshot(self):
         '''Take screenshot of an app'''
@@ -61,53 +61,26 @@ class Auto_ss():
         self.saveDC.SelectObject(self.saveBitMap)
 
         # if screen taken succesfully, result == 1
-        result = windll.user32.PrintWindow(self.hwnd, self.saveDC.GetSafeHdc(), 3)
+        result = windll.user32.PrintWindow(
+            self.hwnd, self.saveDC.GetSafeHdc(), 3)
 
         # put window on foreground
         # win32gui.SetForegroundWindow(hwnd)
         bmpinfo = self.saveBitMap.GetInfo()
         bmpstr = self.saveBitMap.GetBitmapBits(True)
 
-        # Create image 
+        # Create image
         im = Image.frombuffer(
             'RGB',
             (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
             bmpstr, 'raw', 'BGRX', 0, 1)
-        
+
         # if screenshot taken succesfully return image, else exit script
         if result == 1:
             return im
         else:
             print("WARNING! Couldn't take screenshot")
             exit()
-
-
-    def _get_counter(self):
-        '''Read counter value saved to file'''
-
-        try:
-            with open("counter.txt", 'r') as file:
-                self.image_count = int(file.read())
-        except:
-            self.image_count = 0
-
-
-    def __init__(self) -> None:
-        '''Get all needed data to make screenshots of one app'''
-
-        # self.window_name = '| microsoft teams'
-        self.window_name = "opera"
-        self.toplist, self.winlist = [], []
-
-        # uncomment when you have your app on main screen
-        # self.w, self.h = screen_size()
-        self.w, self.h = 1920, 1080
-        self.min_similarity = 0.95
-
-        self._get_window_handle()
-        self._create_context()
-        self.saveBitMap = win32ui.CreateBitmap()
-        self._get_counter()
 
     def save_screenshot(self, image):
         '''Save image and counter'''
@@ -122,7 +95,38 @@ class Auto_ss():
 
         except:
             print("couldn't save")
-            
+
+    def manual_screenshot(self):
+        '''Take screenshot manually, without checking similarities'''
+
+        image = self.take_screenshot()
+        self.save_screenshot(image)
+
+    def _get_counter(self):
+        '''Read counter value saved to file'''
+
+        try:
+            with open("counter.txt", 'r') as file:
+                self.image_count = int(file.read())
+        except:
+            self.image_count = 0
+
+    def __init__(self) -> None:
+        '''Get all needed data to make screenshots of one app'''
+
+        # self.window_name_to_search = '| microsoft teams'
+        self.window_name_to_search = "opera"
+        self.toplist, self.winlist = [], []
+
+        # uncomment when you have your app on main screen
+        # self.w, self.h = screen_size()
+        self.w, self.h = 1920, 1080
+        self.min_similarity = 0.95
+
+        self._get_window_handle()
+        self._create_context()
+        self.saveBitMap = win32ui.CreateBitmap()
+        self._get_counter()
 
     def simple_error(self, imageA, imageB):
         '''Calculate how many differences are in particular pixels on two images'''
@@ -131,23 +135,20 @@ class Auto_ss():
         im2_arr = asarray(imageB)
 
         differences = count_nonzero(im1_arr != im2_arr)
-        # wszystkie = w * h * 3
-        
+
         width, height = self.w, self.h
-        # all subpixels 
         all_subpixels = width * height * 3
-        print(differences/(all_subpixels))
+        print("Similarity: " + str((1 - (differences / (all_subpixels))) * 100)+"%")
 
         if self.min_similarity == 1:
             self.save_screenshot(imageB)
             return True
 
-
         # if similarity is lesser than given % save img and return
-        if (differences / all_subpixels) >= ( 1 - self.min_similarity ):
+        if (differences / all_subpixels) >= (1 - self.min_similarity):
             self.save_screenshot(imageB)
             return True
-        
+
         return False
 
     def __del__(self):
@@ -163,7 +164,7 @@ class Auto_ss():
 
         control.key_pressed = True
         if number > -1:
-            self.image_count = number   
+            self.image_count = number
 
     def change_sensitivity(self, value):
         '''Change needed similarity between two images'''
@@ -173,7 +174,7 @@ class Auto_ss():
                 print("Max sensitivity")
             else:
                 print("Min sensitivity")
-            return  
+            return
 
         if value:
             if self.min_similarity >= 0.99:
@@ -185,7 +186,7 @@ class Auto_ss():
                 self.min_similarity -= 0.001
             else:
                 self.min_similarity -= 0.01
-        
-        round(self.min_similarity, 3)
+
+        self.min_similarity = round(self.min_similarity, 3)
 
         print("Current needed similarity: "+str(self.min_similarity*100)+"%")
--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import keyboard
+from os import system
 from auto_ss import *
 from loop_control import *
 from atexit import register
 from time import sleep, time
 
 
+def print_menu(auto_ss):
+    '''Print hotkeys and stats'''
+    system("cls")
+    print("Window name: "+auto_ss.window_name_found)
+    print('''Hotkeys:
+* ctrl+p to pause/continue
+* ctrl+s to take screenshot manually
+* ctrl++ to increase sensitivity
+* ctrl+- to decrease sensitivity
+    ''')
+    print("Screenshot count: "+str(auto_ss.image_count))
+    print("Current sensitivity: "+str(auto_ss.min_similarity*100)+"%")
+
+
 if __name__ == "__main__":
+
+    system('cls')
 
     auto_ss = Auto_ss()
     control = LoopControl()
@@ -16,7 +33,7 @@ if __name__ == "__main__":
     print("Press 0 to start naming from 0, space to continue from previous run")
     print("Default: continue numeration\n")
 
-    for sec in range(5,0,-1):
+    for sec in range(5, 0, -1):
         if control.key_pressed:
             control.key_pressed = False
             break
@@ -30,14 +47,10 @@ if __name__ == "__main__":
     image1 = auto_ss.take_screenshot()
     auto_ss.save_screenshot(image1)
 
-    print('''\nPress ctrl+p to pause/continue
-ctrl++ to increase sensitivity
-ctrl+- to decrease sensitivity
-    ''')
-    
     keyboard.add_hotkey('ctrl+p', control.pause)
-    keyboard.add_hotkey('ctrl+plus', auto_ss.change_sensitivity, args=[True,])
-    keyboard.add_hotkey('ctrl+-', auto_ss.change_sensitivity, args=[False,])
+    keyboard.add_hotkey('ctrl+s', auto_ss.manual_screenshot)
+    keyboard.add_hotkey('ctrl+plus', auto_ss.change_sensitivity, args=[True, ])
+    keyboard.add_hotkey('ctrl+-', auto_ss.change_sensitivity, args=[False, ])
 
     while True:
 
@@ -45,26 +58,22 @@ ctrl+- to decrease sensitivity
 
             # sleep to ignore animations
             sleep(1)
+            print_menu(auto_ss)
 
-            # start = time()
             image2 = auto_ss.take_screenshot()
+            # start = time()
 
-            #compare images
+            # compare images
             saved = auto_ss.simple_error(image1, image2)
-
-            if saved:
-                image1 = image2
 
             # end = time()
             # print("time: ", end - start)
+
+            if saved:
+                image1 = image2
 
         print("PAUSE")
         while control.paused:
             sleep(0.2)
 
         print("CONTINUE")
-        
-
-
-
-
