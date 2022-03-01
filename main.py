6c833cc2c1ad0886da20f1962ff3aa8a5a50332c
SzymekN <nowak.szymek00@gmail.com>
2022-03-01 09:45:31 +0100

    comments, new requirements

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-import time
 import win32gui
 import win32ui
-import atexit
 import keyboard
+from atexit import register
+from time import sleep, time
 from pyautogui import size as screen_size
+from PIL import Image as Image
 from numpy import asarray
 from ctypes import windll
-from PIL import Image as Image
 
 def goodbye(auto_ss):
-
+    '''Function executed at exit, saves counter to file'''
     try:
         with open("counter.txt", 'w') as file:
             file.write(str(auto_ss.image_count))
@@ -18,71 +18,104 @@ def goodbye(auto_ss):
             
 
 class Auto_ss():
+    '''Main class in script, handles finding wanted window and making screen shots'''
 
     def enum_cb(self, hwnd, results):
+        '''Function getting information about all procesess and it's handles'''
+
         self.winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
 
-    def _get_window_handle(self):
-        win32gui.EnumWindows(self.enum_cb, self.toplist)
-        teams = [(hwnd, title) for hwnd, title in self.winlist if self.window_name in title.lower()]
-        teams = teams[0]
-        print(teams)
-        self.hwnd = teams[0]
 
-    def _get_display_handle(self):
+    def _get_window_handle(self):
+        '''Function gets handle to wanted window, and saves it'''
+
+        # get all windows
+        win32gui.EnumWindows(self.enum_cb, self.toplist)
+        
+        # get windows with matching title
+        windows = [(hwnd, title) for hwnd, title in self.winlist if self.window_name in title.lower()]
+
+        # error if couldn't find any window
+        if not windows:
+            print("ERROR! No windows found")
+            exit()
+
+        # choose first found window with matching title
+        window = windows[0]
+        print(window)
+
+        # handle to wanted window
+        self.hwnd = window[0]
+
+
+    def _create_context(self):
+        '''Create new context from window handle'''
+
         self.hwndDC = win32gui.GetWindowDC(self.hwnd)
         self.mfcDC  = win32ui.CreateDCFromHandle(self.hwndDC)
         self.saveDC = self.mfcDC.CreateCompatibleDC()
 
-    def take_screenshot(self):
 
+    def take_screenshot(self):
+        '''Take screenshot of an app'''
+
+        # Create bitmap compatibile with window DC
         self.saveBitMap.CreateCompatibleBitmap(self.mfcDC, self.w, self.h)
         self.saveDC.SelectObject(self.saveBitMap)
 
-        # Change the line below depending on whether you want the whole window
-        # or just the client area. 
+        # if screen taken succesfully, result == 1
         result = windll.user32.PrintWindow(self.hwnd, self.saveDC.GetSafeHdc(), 3)
 
+        # put window on foreground
         # win32gui.SetForegroundWindow(hwnd)
         bmpinfo = self.saveBitMap.GetInfo()
         bmpstr = self.saveBitMap.GetBitmapBits(True)
 
+        # Create image 
         im = Image.frombuffer(
             'RGB',
             (bmpinfo['bmWidth'], bmpinfo['bmHeight']),
             bmpstr, 'raw', 'BGRX', 0, 1)
         
+        # if screenshot taken succesfully return image, else exit script
         if result == 1:
             return im
         else:
             print("WARNING! Couldn't take screenshot")
             exit()
 
+
     def _get_counter(self):
+        '''Read counter value saved to file'''
+
         try:
             with open("counter.txt", 'r') as file:
                 self.image_count = int(file.read())
         except:
             self.image_count = 0
 
+
     def __init__(self) -> None:
+        '''Get all needed data to make screenshots of one app'''
 
         # window_name = '| microsoft teams'
         self.window_name = "opera"
         self.toplist, self.winlist = [], []
         self.w, self.h = screen_size()
+        self.min_similarity = 0.95
 
         self._get_window_handle()
-        self._get_display_handle()
+        self._create_context()
         self.saveBitMap = win32ui.CreateBitmap()
         self._get_counter()
 
     def simple_error(self, imageA, imageB):
+        '''Calculate how many differences are in particular pixels on two images'''
 
         im1_arr = asarray(imageA)
         im2_arr = asarray(imageB)
         
-        width, height = imageA.size
+        width, height = self.w, self.h
         pixel_count = width * height
         similarities = pixel_count
 
@@ -95,22 +128,23 @@ class Auto_ss():
                     similarities -= 1
 
             # if similarity is lesser than given % same img and return
-            if similarities / pixel_count < 0.95:
+            if similarities / pixel_count < self.min_similarity:
 
-                print(similarities / pixel_count)
                 self.image_count += 1
                 imageB.save("ss"+str(self.image_count)+".png")
                 return
 
     def __del__(self):
+        '''Delete used contexts'''
 
         win32gui.DeleteObject(self.saveBitMap.GetHandle())
         self.saveDC.DeleteDC()
         self.mfcDC.DeleteDC()
         win32gui.ReleaseDC(self.hwnd, self.hwndDC)
 
-    def set_counter(self, control, number):
-        
+    def reset_counter(self, control, number):
+        '''Force setting image count to 0'''
+
         control.key_pressed = True
         if number > -1:
             self.image_count = number   
@@ -130,10 +164,10 @@ if __name__ == "__main__":
 
     auto_ss = Auto_ss()
     control = LoopControl()
-    atexit.register(goodbye, auto_ss)
+    register(goodbye, auto_ss)
 
-    keyboard.add_hotkey('space', auto_ss.set_counter, args=[control, -1])
-    keyboard.add_hotkey('0', auto_ss.set_counter, args=[control, 0])
+    keyboard.add_hotkey('space', auto_ss.reset_counter, args=[control, -1])
+    keyboard.add_hotkey('0', auto_ss.reset_counter, args=[control, 0])
     print("Press 0 to start naming from 0, space to continue from previous run")
     print("Default: continue numeration\n")
 
@@ -142,7 +176,7 @@ if __name__ == "__main__":
             control.key_pressed = False
             break
         print(sec, end=".. ", flush=True)
-        time.sleep(1)
+        sleep(1)
 
     keyboard.unregister_all_hotkeys()
 
@@ -157,19 +191,22 @@ if __name__ == "__main__":
     while True:
 
         while control.run:
-            time.sleep(1)
-            start = time.time()
+            sleep(1)
+            start = time()
 
             image2 = auto_ss.take_screenshot()
+
+            #compare images
             auto_ss.simple_error(image1, image2)
+            
             image1 = image2
 
-            end = time.time()
+            end = time()
             print("time: ", end - start)
 
         print("PAUSE")
         while control.paused:
-            time.sleep(0.2)
+            sleep(0.2)
 
         print("CONTINUE")
         
